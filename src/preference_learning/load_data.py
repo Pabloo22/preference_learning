373c1be16231fef890e11d504fdd1ffb6add72e1
Pabloo22 <pablete.arino@gmail.com>
2023-05-10 18:00:49 +0200

    Fix

--- src/preference_learning/load_data.py
+++ src/preference_learning/load_data.py
@@ -34,13 +34,10 @@ def load_dataframe(mode: str = "raw") -> Union[pd.DataFrame, TupleOfDataFrames]:
     y_train_path = project_path / "data" / "y_train.csv"
     y_test_path = project_path / "data" / "y_test.csv"
 
-    X_columns = ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
-    y_columns = ["class"]
-
-    X_train = pd.read_csv(X_train_path, names=X_columns)
-    X_test = pd.read_csv(X_test_path, names=X_columns)
-    y_train = pd.read_csv(y_train_path, names=y_columns)
-    y_test = pd.read_csv(y_test_path, names=y_columns)
+    X_train = pd.read_csv(X_train_path)
+    X_test = pd.read_csv(X_test_path)
+    y_train = pd.read_csv(y_train_path)
+    y_test = pd.read_csv(y_test_path)
 
     return X_train, X_test, y_train, y_test
 
